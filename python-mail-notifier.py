11b512c2902d36750eade04a1a585a78df535bda
Brisppy <49708988+Brisppy@users.noreply.github.com>
2020-07-24 14:15:08 +1000

    Update python-mail-notifier.py
    Added the ability to set a retry count, and modify the default timeout on connection attempts. Improved exception handling.

--- python-mail-notifier.py
+++ python-mail-notifier.py
@@ -4,17 +4,29 @@ import ssl
 import sys
 from email.mime.text import MIMEText
 
+# Set the number of attempts to make
+retrycount = 10
+timeout_secs = 10
+
 context = ssl.create_default_context()
-try:
-	server = smtplib.SMTP(sys.argv[1], sys.argv[2])
-	server.starttls(context=context)
-	server.login(sys.argv[3], sys.argv[4])
-	message = MIMEText(sys.argv[7])
-	message['Subject'] = sys.argv[6]
-	message['From'] = sys.argv[3]
-	message['To'] = sys.argv[5]
-	server.sendmail(sys.argv[3], sys.argv[5], message.as_string())
-except Exception as e:
-	print(e)
-finally:
-	server.quit
+for i in range(1, retrycount):
+	try:
+		print("Attempt", i)
+		server = smtplib.SMTP(sys.argv[1], sys.argv[2], timeout=timeout_secs)
+		server.starttls(context=context)
+		# server.set_debuglevel(1) # Uncomment to view SMTP communication.
+		server.login(sys.argv[3], sys.argv[4])
+		message = MIMEText(sys.argv[7])
+		message['Subject'] = sys.argv[6]
+		message['From'] = sys.argv[3]
+		message['To'] = sys.argv[5]
+		server.sendmail(sys.argv[3], sys.argv[5], message.as_string())
+	except smtplib.SMTPException as e:
+		print(e)
+		break
+	except smtplib.socket.error as e:
+		print(e)
+		continue
+	else:
+		server.quit
+		break
